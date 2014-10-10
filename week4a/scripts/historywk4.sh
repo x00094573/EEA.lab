1cf25102375acff88c5a03fc974cb0223ccec8bc
x00094573 <x00094573@ittd.ie>
2014-10-10 11:56:48 +0100

    week 4

---
+++ week4a/scripts/historywk4.sh
@@ -0,0 +1,181 @@
+    1  ls -ltr
+    2  sudo gedit /etc/cntlm.conf 
+    3  bg
+    4  cd
+    5  find . -name *.crt -print 2>/dev/null
+    6  cd Downloads/
+    7  ls
+    8  vi eduroam-linux-IoTT_Linux.sh 
+    9  cd
+   10  sudo gedit /etc/cntlm.conf 
+   11  sudo gedit /etc/cntlm.conf 
+   12  bg
+   13  cntlm -M
+   14  cntlm -H
+   15  cntlm -H
+   16  cntlm -H
+   17  cntlm -H
+   18  sudo service cntlm restart
+   19  cntlm -H
+   20  cntlm -H cntlm.conf
+   21  cntlm cntlm.conf -H
+   22  cntlm -H
+   23  cntlm -H www.sun.com
+   24  cntlm -H
+   25  cntlm -H
+   26  ls /etc
+   27  sudo mv /etc/cntlm.conf /etc/cntlm.conf.old
+   28  cntlm -H
+   29  ls
+   30  sudo apt-get remove cntlm
+   31  sudo apt-get install cntlm
+   32  sudo apt-get remove cntlm
+   33  sudo apt-get install cntlm
+   34  sudo service start cntlm
+   35  sudo apt-get install cntlm
+   36  sudo service start 
+   37  sudo service cntlm start
+   38  sudo service cntlm start -v
+   39  sudo apt-get remove cntlm
+   40  sudo apt-get install cntlm
+   41  sudo apt-get remove cntlm
+   42  sudo apt-get install cntlm
+   43  sudo apt-get remove cntlm
+   44  find / -name cntlm.conf -print
+   45  find / -name cntlm.conf -print 2>/dev/nul
+   46  find / -name cntlm.conf -print 2>/dev/null
+   47  sudo apt-get remove cntlm
+   48  sudo apt-get install cntlm
+   49  sudo apt-get install cntlm
+   50  ls /etc/cntlm.conf
+   51  sudo apt-get purge cntlm
+   52  sudo apt-get install cntlm
+   53  find / -name cntlm.conf -print 2>/dev/null
+   54  cntlm -H
+   55  sudo vi /etc/cntlm.conf
+   56  cntlm -H
+   57  fg
+   58  sudo service cntlm restart
+   59  vi eduroam-linux-IoTT_Linux.sh 
+   60  sudo vi /etc/cntlm.conf
+   61  cntlm -M
+   62  cntlm -H -c /etc/cntlm.conf
+   63  cntlm -c /etc/cntlm.conf -H
+   64  cntlm -H
+   65  cntlm -c /etc/cntlm.conf -H
+   66  cntlm -c /etc/cntlm.conf 
+   67  cntlm -H -u jburns@stf
+   68  sudo vi /etc/cntlm.conf
+   69  sudo service cntlm restart
+   70  sudo vi /etc/cntlm.conf
+   71  sudo service cntlm restart
+   72  sudo service cntlm restart
+   73  sudo service cntlm restart
+   74  ls -ltr
+   75  rm *.cer
+   76  ls
+   77  chef
+   78  ls /opt
+   79  /opt/chef/bin/chef-solo
+   80  sudo shutdown -r now
+   81  ls -ltr
+   82  vi .profile 
+   83  export http_proxy=http://localhost:3128
+   84  sudo -E apt-get install git
+   85  git config --global user.email "<x00094573@ittd.ie>"
+   86  Week 2 Lab (a): configuring github
+   87  Open a terminal and run:
+   88  export GIT_SSL_NO_VERIFY=1
+   89  export http_proxy=http://localhost:3128
+   90  sudo -E apt-get install git
+   91  git config --global user.email "x00094573@ittd.ie
+   92  mkdir week4
+   93  cd week4
+   94  touch README.md
+   95  git init
+   96  git add README.md
+   97  git commit -m "first commit"
+   98  git remote add origin https://github.com/x00094573/4th-Year-IT-Management.git
+   99  git push -u origin master
+  100  mkdir scripts
+  101  cd scripts
+  102  man chmod
+  103  gedit hw.sh
+  104  chmod +x hw.sh
+  105  man chmod
+  106  hw.sh
+  107  echo hw.sh
+  108  sh hw.sh
+  109  cd ..
+  110  git add -A
+  111  git commit -a
+  112  git push origin master
+  113  export GIT_SSL_NO_VERIFY=1
+  114  export http_proxy=http://localhost:3128
+  115  git push origin master
+  116  git push origin master
+  117  git hub origin master
+  118  git push origin master
+  119  git push origin master
+  120  git push origin master
+  121  cd EEA.lab
+  122  cd ..
+  123  git add -A
+  124  git commit -a
+  125  git push origin master
+  126  cd week4
+  127  git add -A
+  128  git commit -a
+  129  cd scripts
+  130  git add -A
+  131  git commit -a
+  132  cd ..
+  133  cd ..
+  134  cd ..
+  135  cd 4th-Year-IT-Management
+  136  cd 4th-Year-IT-Management
+  137  git add -A
+  138  cd EEA.lab
+  139  cd ..
+  140  cd EEA.lab
+  141  git add -A
+  142  git commit -a
+  143  git add -A
+  144  git commir -a
+  145  git commit -a
+  146  git push -u origin master
+  147  git add -A
+  148  git commit -a
+  149  git add -A
+  150  git commit -a
+  151  git push origin master
+  152  sh cpucount.sh
+  153  chmod +x
+  154  chmod +x cpucount.sh
+  155  cd week3
+  156  chmod +x cpucount.sh
+  157  sh cpucount.sh
+  158  gedit hwQ5.sh
+  159  cd --
+  160  cd EEA.lab
+  161  cd week4a
+  162  git add -A
+  163  git commit -a
+  164  git push -u origin master
+  165  export GIT_SSL_NO_VERIFY=1
+  166  export http_proxy=http://localhost:3128
+  167  cd EEA.lab
+  168  cd week4a
+  169  cd scripts
+  170  gedit hwQ5.sh
+  171  chmod +x hwQ5.sh
+  172  sh hwQ5.sh
+  173  sh hwQ5.sh derek
+  174  sh hwQ5.sh 
+  175  sh hwQ5.sh 
+  176  chmod +x hwQ5.sh
+  177  sh hwQ5.sh
+  178  sh hwQ5.sh
+  179  sh hwQ5.sh
+  180  sh hwQ5.sh
+  181  history > historywk4.sh
---
+++ week4a/scripts/hwQ5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#: Title : hello world 
+#: Date : 10/10/2014
+#: Author :Derek Hughes 
+#: Version :1
+#: Description :printing to screen
+#: Options : 
+
+echo "Hello, What is your name ?" 
+read input_variable1
+echo "The month you were born ?" 
+read input_variable2
+echo "Your favourite colour ?" 
+read input_variable3
+echo "hello $input_variable1, you were born in $input_variable2, your favourite colour is $input_variable3"
+
