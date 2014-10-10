8106dbd2d155705683eaadeae080002f765646ef
x00094573 <x00094573@ittd.ie>
2014-10-10 11:09:37 +0100

    hello week4

---
+++ week 4a/scripts/hw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title : hello world 
+#: Date : 10/10/2014
+#: Author :Derek Hughes 
+#: Version :1
+#: Description :printing to screen
+#: Options : 
+
+printf "%s\n" "Hello, World!"
